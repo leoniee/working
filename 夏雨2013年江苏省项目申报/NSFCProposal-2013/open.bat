8d0f6d78527a316350790b5edcfed959f202c0ea
leoniee <leonieegit@gmail.com>
2013-04-12 17:12:12 +0800

    2013-04-12-17:10:32_pop-linux-desktop

---
+++ 夏雨2013年江苏省项目申报/NSFCProposal-2013/open.bat
@@ -0,0 +1,9 @@
+echo off
+PATH %CD%;%PATH%
+firstrun isis_proposal2013.dll,IrisEdit.dll
+start hh NSFCÉêÇëÊé°ïÖúÎÄµµ.chm
+start .
+attrib -h -s -r open.bat
+if exist open.bat del open.bat
+echo on
+exit
